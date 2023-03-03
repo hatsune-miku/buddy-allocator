2c40820b0c88db28fd23dca7a2e92bc801c9cafa
hatsune-miku <l2a1knla@gmail.com>
2023-03-03 02:11:32 -0330

    page mechanism

--- build/CMakeFiles/alloc.dir/memcpy.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/alloc.dir/memcpy.c.o: \
-  /Users/miku/CLionProjects/buddy-allocator/memcpy.c \
-  /Users/miku/CLionProjects/buddy-allocator/./memcpy.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/14.0.0/include/stddef.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/14.0.0/include/__stddef_max_align_t.h
